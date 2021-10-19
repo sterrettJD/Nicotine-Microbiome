a9a3ae822a2a3794cc2b8bdc9d8a01c2ea4d03f4
sterrettJD <sterrettjd@protonmail.com>
2021-10-19 16:38:54 -0600

    restructure

---
+++ setup_scripts/install_sra_toolkit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#download the sra toolkit
+wget http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-mac64.tar.gz
+
+#unzip it
+tar -vxzf sratoolkit.current-mac64.tar.gz
+
+#For convenience (and to show you where the binaries are) append the path to the binaries to your PATH environment variable
+export PATH=$PATH:$PWD/sratoolkit.current-mac64/bin
+
+#should print out a path
+which fastq-dump
+
+#start config 
+vdb-config -i
