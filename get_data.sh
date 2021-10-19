4ddf101a8dfccb3f27a196120583aa495250c975
sterrettJD <sterrettjd@protonmail.com>
2021-10-19 17:00:46 -0600

    update project path, show download progress

--- get_data.sh
+++ get_data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-filename='SRR_Acc_List.txt'
+filename='PRJNA548383_Acc_List.txt'
 
 while read line;
 do
 # for read each line
 
-fasterq-dump $line -outdir PRJNA548383/$line
+fasterq-dump $line -outdir PRJNA548383/$line --progress
 
 done < $filename
