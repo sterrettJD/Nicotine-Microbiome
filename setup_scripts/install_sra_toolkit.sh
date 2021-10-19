7d3a50d14fcb62b7204dbb75350335e54a5fde9c
sterrettJD <sterrettjd@protonmail.com>
2021-10-19 16:42:01 -0600

    fix export path

--- setup_scripts/install_sra_toolkit.sh
+++ setup_scripts/install_sra_toolkit.sh
@@ -7,7 +7,7 @@ wget http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-mac64.
 tar -vxzf sratoolkit.current-mac64.tar.gz
 
 #For convenience (and to show you where the binaries are) append the path to the binaries to your PATH environment variable
-export PATH=$PATH:$PWD/sratoolkit.current-mac64/bin
+export PATH=$PATH:$PWD/sratoolkit.2.11.2-mac64/bin
 
 #should print out a path
 which fastq-dump
