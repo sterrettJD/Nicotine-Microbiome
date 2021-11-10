415dfdb80a39b7232ad91206f7498e8c74a44af3
sterrettJD <sterrettjd@protonmail.com>
2021-11-10 09:23:43 -0700

    read diamond func, add to diamond partial func

--- diamond/map_uniprot_hits.py
+++ diamond/map_uniprot_hits.py
@@ -1,5 +1,25 @@
 import urllib.parse
 import urllib.request
+import pandas as pd
+
+
+def read_diamond_df(filepath):
+    diamond_columns = ["Query accession",
+                        "Target accession",
+                        "Sequence identity",
+                        "Length",
+                        "Mismatches",
+                        "Gap openings",
+                        "Query start",
+                        "Query end",
+                        "Target start",
+                        "Target end",
+                        "E-value",
+                        "Bit score"]
+
+    diamond_df = pd.read_csv(filepath, sep="\t", header=None)
+    diamond_df.columns = diamond_columns
+    return diamond_df
 
 def get_gene_name(query_list):
     url = 'https://www.uniprot.org/uploadlists/'
@@ -18,9 +38,20 @@ def get_gene_name(query_list):
         response = f.read()
     return response.decode('utf-8')
 
-def main():
-    response = get_gene_name(["A0A009SNI0", "A0A084FU31"])
-    print(response)
 
+def add_to_diamond(responses):
+    # using [1:] here drops the "from \t to" in the reponse 
+    responses_list = responses.split("\n")[1:]
+    response_df = pd.DataFrame([x.split("\t") for x in responses_list], 
+                                columns=["query", "name"])
+
+    return response_df
+
+def main():
+    responses = get_gene_name(["A0A009SNI0", "A0A084FU31"])
+    diamond = read_diamond_df("organisms/Paenarthrobacter_diamond_results.txt")
+
+    #df = add_to_diamond(responses, filepath="organisms/Paenarthrobacter_diamond_results.txt")
+    print(diamond)
 if __name__=="__main__":
     main()
