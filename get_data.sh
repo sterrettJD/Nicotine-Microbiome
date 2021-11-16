37add2bc06d14738b7a6155e9e62616386ee1289
sterrettJD <jost9358@colorado.edu>
2021-11-15 19:41:02 -0700

    Modify to SLURM compat

--- get_data.sh
+++ get_data.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
+#SBATCH -p short # Partition or queue. In this case, short!
+#SBATCH --job-name=download_data # Job name
+#SBATCH --mail-type=ALL # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=jost9358@colorado.edu
+#SBATCH --nodes=1 # Only use a single node
+#SBATCH --ntasks=1 # Run on a single CPU
+#SBATCH --mem=100gb # Memory limit
+#SBATCH --time=04:00:00 # Time limit hrs:min:sec
+#SBATCH --output=/Users/jost9358/Nicotine-Microbiome_%j.out # Standard output and error log
+#SBATCH --error=/Users/jost9358/Nicotine-Microbiome_%j.err # %j inserts job number
 
 
 for filename in PRJNA508385_Acc_List.txt  PRJNA544061_Acc_List.txt  PRJNA548383_Acc_List.txt ; do
