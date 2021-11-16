78c4cab6848ae60e26d8455a38547f7161fdd993
Jost Sterrett <jost9358@fiji-1.colorado.edu>
2021-11-15 19:29:36 -0700

    add for loop for accession lists

--- get_data.sh
+++ get_data.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-filename='PRJNA548383_Acc_List.txt'
 
-while read line;
-do
-# for read each line
 
-prefetch $line --output-directory PRJNA548383/$line
-fasterq-dump $line --outdir PRJNA548383/$line --progress
+for filename in PRJNA508385_Acc_List.txt  PRJNA544061_Acc_List.txt  PRJNA548383_Acc_List.txt ; do
 
-done < $filename
+    while read line;
+    do
+    # for read each line
+
+    prefetch $line --output-directory PRJNA548383/$line
+    fasterq-dump $line --outdir PRJNA548383/$line --progress
+
+    done < Accession_lists/$filename
+
+done
