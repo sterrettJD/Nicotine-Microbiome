7106f23f1c7f759f2e1813c027a2c53801b4f908
sterrettJD <jost9358@colorado.edu>
2021-11-21 19:22:40 -0700

    Not sure these files matter anymore but let's keep them for now

--- diamond/run_snakemake.sh
+++ diamond/run_snakemake.sh
@@ -11,8 +11,11 @@ cluster+="--time 10:00:00 "
 cluster+="--nodes 1 "
 cluster+="--ntasks-per-node 20 " 
 cluster+="--mem 260gb "
+cluster+="--wait 60" 
 cluster+="--output /Users/jost9358/Nicotine-Microbiome/run_diamond_out/%j.out "
-cluster+="--error /Users/jost9358/Nicotine-Microbiome/run_diamond_out/%j.err "
+cluster+="--error /Users/jost9358/Nicotine-Microbiome/run_diamond_out/%j.err"
 cluster+="\""
 
-snakemake --cluster "sbatch --account jost9358 --partition short" -j 20
+snakemake --cluster $cluster -j 20 --latency-wait 60
+
+#echo $cluster
